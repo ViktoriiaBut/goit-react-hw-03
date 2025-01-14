d7512f8c34c0a8299881ea13a886553ee607fff4
ViktoriiaBut <zironka.tory@gmail.com>
2025-01-14 21:05:47 +0000

    f_f

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from './Components/ContactList/ContactList';
+import ContactList from "./components/ContactList/ContactList";
 
 
 
